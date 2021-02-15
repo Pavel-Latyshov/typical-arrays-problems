4076b7c8b2234c85590c9ac0948b42dddb7833d6
Pavel Latyshov <platyshov@MacBook-Pro-Pavel.local>
2021-02-15 20:35:40 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    let arr = array
+      .sort((a, b) => a - b)
+      .shift()
+    return arr
+  }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    let arr = array
+      .sort((a, b) => a - b)
+      .pop()
+    return arr
+  }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length == 0) {
+    return 0
+  } else {
+    let l = array.length
+    let sum = array.reduce((total, num) => total + num, 0)
+    let average = sum / l
+    return average
+  }
 }
+
